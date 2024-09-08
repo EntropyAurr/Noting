a638c5b1d322a19e99ef981aaf4ccef2a0b6e9cc
Aurora <auduong.301@gmail.com>
2024-09-08 19:08:53 +0700

    jekyll

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 source "https://rubygems.org"
-gem 'nokogiri'
-gem 'rack', '~> 2.2.4'
-gem 'rspec'
+gem 'jekyll'
 
 # gem "rails"
---
+++ _site/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Ubuntu: ["Ubuntu", "system-ui"],
+        Merienda: ["Merienda", "system-ui"],
+        "Shantell-Sans": ["Shantell Sans", "system-ui"],
+        "Playpen-Sans": ["Playpen Sans", "system-ui"],
+        Play: ["Play", "system-ui"],
+        Niramit: ["Niramit", "system-ui"],
+        "Nanum-Gothic-Coding": ["Nanum Gothic Coding", "system-ui"],
+        Mukta: ["Mukta", "system-ui"],
+        KoHo: ["KoHo", "system-ui"],
+        "Baloo-Bhai-2": ["Baloo Bhai 2", "system-ui"],
+        Acme: ["Acme", "system-ui"],
+        "Doppio-One": ["Doppio One", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+  prefix: "tw-",
+};
